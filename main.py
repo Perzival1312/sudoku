b5f165fa6507a6ede1c6aac1e18fd7866851bf39
Perzival1312 <jj.mongoose11@hotmail.com>
2020-03-18 15:52:54 -0400

    psuedocode and func defs

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ functions:
     row checker
     column checker
     box checker
+    board checker
     replace func (prolly custom?) <-- look this up...
     print the solved board out
 
@@ -21,27 +22,30 @@ output:
 next:
     implement with pygame for better visuals
     optimization!
+'''
 
-
+'''
+Dani suggestion/approval...
+I think a sudoku game would be within scope for this intensive,
+and the solver could be your bike or car, approved
 '''
 
 
-
-
-
 import pygame
-print('Hello, World!')
 
 def parse_input(input):
+    '''Take the input string from the command line and convert it into something usable'''
     usable = []
     return usable
 
 def row_checker(board, num, row):
+    '''Check if the number (num) is in the specified row (row)'''
     if num in board[row-1]:
         return False
     return True
 
 def col_checker(board, num, col):
+    '''Check if the number (num) is in the specified column (col)'''
     for row in board:
         if num == row[col+1]:
             return False
@@ -54,11 +58,34 @@ def box_checker(board, num, box):
         b4  b5  b6
         b7  b8  b9
 
-    floor(box/3), box%3 = row, col - of the specific box
+    find specific box placement in board array:
+    
+    ciel(box/3) = row
+    box%3 = col
+
+    (row-1)*3 = west boundary
+    (row*3)-1 = east boundary
+
+    (col-1)*3 = north boundary
+    (col*3)-1 = south boundary
+
+
+
+    OR
+
+    maually store all coordinates of the board in a dict where key is box number
+    and theres a list of coordinates as the values
+    ^ Should that be hard coded in or algorithmically created at every startup?
+    well if were only doing 9x9 it wouuld be fine to hard code it 
+    but that looks atrocious...
     
     '''
+    pass
 
+def board_checker(board):
+    '''Final check to make sure that the board is properly solved'''
     pass
 
 def printer(board):
+    '''Print the board to the terminal formatted in a TBD way'''
     pass
